e2492a8eac9d9891a6bf03d4b673f21136ccefe1
chuanqin <chuanqing.qin@nokia-sbell.com>
2018-11-14 22:18:24 +0800

    re-factor code, put the const message to sepearte floder

---
+++ com/qcq/const/media_id.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Created on Nov 14, 2018
+
+@author: qinchuanqing
+'''
+media_id_me = u'us79WMrDF_ujGvrA5fvMnAlawfw27AWsngXo07WQIuJqdiSApFfACo4Gi3HWHqSR'
---
+++ com/qcq/const/message.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Created on Nov 14, 2018
+
+@author: qinchuanqing
+'''
+subscription_content = u'欢迎你关注我的公众号，你一定是我的老朋友，我有酒你有故事吗。'
+default_content = u"你好，我是你的老朋友，这是我开发的公众号。准备提供电子书服务，希望你喜欢。"
