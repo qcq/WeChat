febfa265047fcfc81104ae39d5e06c4b311008eb
Qin <chuanqing.qin@nokia-sbell.com>
2018-11-22 18:11:19 +0800

    add the database support in docker with postgres

--- com/qcq/handles/index.py
+++ com/qcq/handles/index.py
@@ -10,12 +10,9 @@ import sys
 import web
 
 render = web.template.render(os.path.dirname(sys.argv[0]) + '../templates/')
-db = web.database(dbn='postgres', user='postgres', pw='root', db='ebook')
+db = web.database(dbn='postgres', user='postgres', host = "172.17.0.4", pw='root', db='ebook')
 
 class Index:
     def GET(self):
         webData = web.input(name=None)
-        if webData:
-            return render.index(webData.name)
-        else:
-            return render.index(db.select('todo'))
+        return render.index(webData.name, db.select('todo'))
