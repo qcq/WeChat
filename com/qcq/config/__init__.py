c6c43ad9a7668b9e78b35d4763a3adf28aaf2851
Qin <chuanqing.qin@nokia-sbell.com>
2018-12-04 17:00:08 +0800

    fix the project which can email with python for attachment

--- com/qcq/config/__init__.py
+++
@@ -1,6 +0,0 @@
-import configparser
-settings = configparser.ConfigParser()
-settings._interpolation = configparser.ExtendedInterpolation()
-settings.read('config.ini')
-print settings.sections()
-print settings.get(u'email section', 'email user')
--- com/qcq/email/emailUtils.py
+++ com/qcq/email/emailUtils.py
@@ -9,25 +9,40 @@ Created on 2018年12月4日
 #===============================================================================
 # below original code comes from the link:https://blog.csdn.net/gpf951101/article/details/78909233
 # great thanks to this great man. I modify the code to suit my own project.
+# also reference for attachment of email https://github.com/rootzhongfengshan/python_practical/blob/master/SentMail/SentMailWithAttachment.py
 #===============================================================================
 
 import smtplib
+import traceback
 from email.mime.text import MIMEText
-from com.qcq.config import settings
+from email.mime.multipart import MIMEMultipart
+from com.qcq.config.config import settings
 
 
-def send_mail(recv, title, content, mail_host='smtp.163.com', port=465):
+def send_mail(recv, title, content, attachments, mail_host='smtp.163.com', port=465):
     username = settings.get(u'email section', u'email user')
     passwd = settings.get(u'email section', u'email password')
-    msg = MIMEText(content)
-    msg['Subject'] = title
-    msg['From'] = username
-    msg['To'] = recv
-    print 'Begin Connect...'
-    smtp = smtplib.SMTP_SSL(mail_host, port=port)
-    print 'Begin Login...'
-    smtp.login(username, passwd)
-    print 'Begin Send...'
-    smtp.sendmail(username, recv, msg.as_string())
-    smtp.quit()
-    print('email send success.')
+    content = '<h1>'+content+'</h1><p>WeChat Official account QCQ.</p>' 
+    msg = MIMEText(content, 'html')
+    
+    message = MIMEMultipart()
+    message['Subject'] = title
+    message['From'] = username
+    message['To'] = recv
+    message.attach(msg)
+    for fileName in attachments:
+        attachment = MIMEText(open(fileName, 'rb').read(), 'base64', 'utf-8')
+        attachment["Content-Type"] = 'application/octet-stream'
+        attachment["Content-Disposition"] = 'attachment; filename="' + fileName + '"'
+        message.attach(attachment)
+    try:
+        print 'Begin Connect...'
+        smtp = smtplib.SMTP_SSL(mail_host, port=port)
+        print 'Begin Login...'
+        smtp.login(username, passwd)
+        print 'Begin Send...'
+        smtp.sendmail(username, recv, message.as_string())
+        smtp.quit()
+        print('email send success.')
+    except Exception, exc:
+        print exc, traceback.print_exc()
