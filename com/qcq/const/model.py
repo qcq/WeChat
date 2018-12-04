3fffd57b14dde44f931503bc831d1e58c5a997af
Qin <chuanqing.qin@nokia-sbell.com>
2018-12-04 10:22:58 +0800

    fix database code

--- com/qcq/const/model.py
+++ com/qcq/const/model.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 '''
-Created on 2018Äê12ÔÂ4ÈÕ
+Created on 2018ï¿½ï¿½12ï¿½ï¿½4ï¿½ï¿½
 
 @author: chuanqin
 '''
 import web, datetime
+import com.qcq.const.webconst as webconst
 
-db = web.database(dbn='mysql', db='blog', user='justin')
 
 def get_posts():
-    return db.select('entries', order='id DESC')
+    return webconst.db.select('entries', order='id DESC')
 
 def get_post(id):
     try:
-        return db.select('entries', where='id=$id', vars=locals())[0]
+        return webconst.db.select('entries', where='id=$id', vars=locals())[0]
     except IndexError:
         return None
 
 def new_post(title, text):
-    db.insert('entries', title=title, content=text, posted_on=datetime.datetime.utcnow())
+    webconst.db.insert('entries', title=title, content=text, posted_on=datetime.datetime.utcnow())
 
 def del_post(id):
-    db.delete('entries', where="id=$id", vars=locals())
+    webconst.db.delete('entries', where="id=$id", vars=locals())
 
 def update_post(id, title, text):
-    db.update('entries', where="id=$id", vars=locals(),
+    webconst.db.update('entries', where="id=$id", vars=locals(),
         title=title, content=text)
