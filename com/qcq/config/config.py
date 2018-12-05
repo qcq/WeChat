d357075d03f5c9007af4bd91aa9bba963f12c67b
Qin <chuanqing.qin@nokia-sbell.com>
2018-12-05 14:28:39 +0800

    fix the config can not found the config.ini file, and delete useless file of media_id.py

--- com/qcq/config/config.py
+++ com/qcq/config/config.py
@@ -5,9 +5,12 @@ Created on 2018��12��4��
 
 @author: chuanqin
 '''
+import os
 import configparser
+import sys
+
 settings = configparser.ConfigParser()
 settings._interpolation = configparser.ExtendedInterpolation()
-settings.read('./config.ini')
+settings.read(os.path.dirname(os.path.abspath(sys.argv[0])) + '/../config/config.ini')
 print settings.sections()
 print settings.get(u'email section', 'email user')
