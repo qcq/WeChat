045dd4914694a54b3d537cadfc79117a2ac50859
Qin <chuanqing.qin@nokia-sbell.com>
2019-01-04 13:37:41 +0800

    fix bug, when already login, just re-direct to todo home page

--- com/qcq/handles/url_mapping.py
+++ com/qcq/handles/url_mapping.py
@@ -74,7 +74,7 @@ class Login:
     def GET(self):
         if logged():
             render = create_render(session.privilege)
-            return '%s' % render.login_double()
+            raise web.seeother('/todo')
         else:
             render = create_render(session.privilege)
             return '%s' % render.login()
